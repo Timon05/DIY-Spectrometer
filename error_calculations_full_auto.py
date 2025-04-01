00b30c7e4795535fdbe871bf9a3f8eb298299610
Timon05 <145014231+Timon05@users.noreply.github.com>
2025-04-01 20:04:07 +0200

    Update error_calculations_full_auto.py

--- error_calculations_full_auto.py
+++ error_calculations_full_auto.py
@@ -1,8 +1,8 @@
 import os
 from error_v3 import main
-for files in os.listdir("./Downloads/photos/originals"):
+for files in os.listdir("./thisfolder/photos/originals"):
   print(files)
   
-  main(path=f"C:\\Users\\timon\\Downloads\\photos\\originals\\{files}",save_path=f"C:\\Users\\timon\\Downloads\\photos\\{files}")
+  main(path=f"path\\to\\folder{files}",save_path=f"path\\to\\folder\\photos{files}")
 
  
